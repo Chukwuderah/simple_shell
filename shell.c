b880c204b47efd8e54f526cb9834501e38434844
estherohiomame <100796143+estherohiomame@users.noreply.github.com>
2023-08-11 19:19:18 +0100

    Update shell.c

--- shell.c
+++ shell.c
@@ -5,32 +5,32 @@
 #include <sys/wait.h>
 
 /**
- * main - PaShell: Simplified Unix Command Line Interpreter
- *
- * Description:
- * This Simple Shell was built as part of an ALX Project.
- *
- * Return: 0, if code ran successfully.
- */
+* main - PaShell: Simplified Unix Command Line Interpreter
+*
+* Description:
+* This Simple Shell was built as part of an ALX Project.
+*
+* Return: 0, if code ran successfully.
+*/
 int main(void)
 {
     while (1)
     {
         write(STDOUT_FILENO, "PaShell $ ", 10);
-
+        
         char command[100];
         if (fgets(command, sizeof(command), stdin) == NULL)
             break;
-
+        
         command[strcspn(command, "\n")] = '\0';
-
+        
         pid_t pid = fork();
-
+        
         if (pid == 0)
         {
             char *args[] = {command, NULL};
             execve(command, args, NULL);
-
+            
             char error_message[100];
             snprintf(error_message, sizeof(error_message), "%s: ", command);
             perror(error_message);
