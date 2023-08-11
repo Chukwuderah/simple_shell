81f0775341b3d6d7010eae9d50847c2d91d0d3f5
estherohiomame <estherohiomame@gmail.com>
2023-08-11 11:33:24 -0700

    Corrections to Task 1

--- shell.c
+++ shell.c
@@ -14,36 +14,39 @@
 */
 int main(void)
 {
-    while (1)
-    {
-        write(STDOUT_FILENO, "PaShell $ ", 10);
-        
-        char command[100];
-        if (fgets(command, sizeof(command), stdin) == NULL)
-            break;
-        
-        command[strcspn(command, "\n")] = '\0';
-        
-        pid_t pid = fork();
-        
-        if (pid == 0)
-        {
-            char *args[] = {command, NULL};
-            execve(command, args, NULL);
-            
-            char error_message[100];
-            snprintf(error_message, sizeof(error_message), "%s: ", command);
-            perror(error_message);
-            _exit(EXIT_FAILURE);
-        }
-        else if (pid < 0)
-        {
-            perror("PaShell");
-        }
-        else
-        {
-            wait(NULL);
-        }
-    }
-    return (0);
+	while (1)
+	{
+		write(STDOUT_FILENO, "PaShell $ ", 10);
+
+		char command[100];
+
+		if (fgets(command, sizeof(command), stdin) == NULL)
+			break;
+
+		command[strcspn(command, "\n")] = '\0';
+
+		pid_t pid = fork();
+
+		if (pid == 0)
+		{
+			char *args[] = {command, NULL};
+
+			execve(command, args, NULL);
+
+			char error_message[100];
+
+			snprintf(error_message, sizeof(error_message), "%s: ", command);
+			perror(error_message);
+			_exit(EXIT_FAILURE);
+		}
+		else if (pid < 0)
+		{
+			perror("PaShell");
+		}
+		else
+		{
+			wait(NULL);
+		}
+	}
+	return (0);
 }
