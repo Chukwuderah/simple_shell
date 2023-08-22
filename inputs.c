121203428ae27a3784c61429bfcd9ed263bb267e
Chukwuderah <pleasantchukwu@gmail.com>
2023-08-22 05:26:23 -0700

    inputs.c

--- inputs.c
+++ inputs.c
@@ -6,7 +6,6 @@
 #include "functions.h"
 #include <stdbool.h>
 
-
 /**
  * tokenize_input - Tokenize the input string into arguments.
  * @input: The input string to tokenize.
@@ -38,44 +37,51 @@ int tokenize_input(char *input, char *args[])
 
 bool find_command(const char *command)
 {
-    if (access(command, X_OK) == 0)
-    {
-        printf("This is a command called %s.\n", command);
-        return true;
-    }
+	if (access(command, X_OK) == 0)
+	{
+		printf("This is a command called %s.\n", command);
+		return (true);
+	}
 
-    char *path = _getenv("PATH");
-    printf("PATH: %s\n", path);
+	char *path = _getenv("PATH");
 
-    if (path != NULL)
-    {
-        printf("Searching for command: %s\n", command);
+	printf("PATH: %s\n", path);
 
-        int num_tokens = 0;
-        char **dirs = _strtok(path, ":");
-	char executable_path[256] = "";
+	if (path != NULL)
+	{
+		printf("Searching for command: %s\n", command);
 
-        for (int i = 0; i < num_tokens; i++)
-        {
-            _strcpy(executable_path, dirs[i]);
-            _strcat(executable_path, "/");
-            _strcat(executable_path, command);
+		int num_tokens = 0;
+		char **dirs = _strtok(path, ":");
+		char executable_path[256] = "";
 
-            printf("Checking path: %s\n", executable_path);
+		for (int i = 0; i < num_tokens; i++)
+		{
+			_strcpy(executable_path, dirs[i]);
+			_strcat(executable_path, "/");
+			_strcat(executable_path, command);
 
-            if (access(executable_path, X_OK) == 0)
-            {
-                printf("Command found: %s\n", executable_path);
-                free_dir_tokens(dirs, num_tokens);
-                return true;
-            }
-        }
+			printf("Checking path: %s\n", executable_path);
 
-        free_dir_tokens(dirs, num_tokens);
-    }
+			if (access(executable_path, X_OK) == 0)
+			{
+				printf("Command found: %s\n", executable_path);
+				free_dir_tokens(dirs, num_tokens);
+				return (true);
+			}
+		}
 
-    return false;
+		free_dir_tokens(dirs, num_tokens);
+	}
+
+	return (false);
 }
+
+/**
+ * free_dir_tokens - Frees the strtok tokens
+ * @dir: The list of tokens
+ * @num_tokens: The number of tokens to be freed
+ */
 
 void free_dir_tokens(char **dirs, int num_tokens)
 {
