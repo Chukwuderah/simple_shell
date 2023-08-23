dc78c457fd4c5c3b051bf6615a7cc6441efb5db7
estherohiomame <100796143+estherohiomame@users.noreply.github.com>
2023-08-23 20:42:51 +0100

    Update strings2.c

--- strings2.c
+++ strings2.c
@@ -19,3 +19,27 @@ char *_strchr(char *string, char character)
 	}
 	return (0);
 }
+
+/**
+ * _strcat - Custom string concatenation function that appends the contents of
+ * the source string to the end of the destination string.
+ *
+ * @dest: The destination string.
+ * @src: The source string.
+ */
+
+void _strcat(char *dest, const char *src)
+{
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+}
