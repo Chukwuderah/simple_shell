a03b7fccf633fd4d720f597af8ff72ba0188f5a5
estherohiomame <100796143+estherohiomame@users.noreply.github.com>
2023-08-23 20:42:20 +0100

    Update strings.c

--- strings.c
+++ strings.c
@@ -108,27 +108,3 @@ int _strncmp(const char *s1, const char *s2, size_t n)
 
 	return (0);
 }
-
-/**
- * _strcat - Custom string concatenation function that appends the contents of
- * the source string to the end of the destination string.
- *
- * @dest: The destination string.
- * @src: The source string.
- */
-
-void _strcat(char *dest, const char *src)
-{
-	while (*dest != '\0')
-	{
-		dest++;
-	}
-
-	while (*src != '\0')
-	{
-		*dest = *src;
-		dest++;
-		src++;
-	}
-	*dest = '\0';
-}
